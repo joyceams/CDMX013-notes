f5bbcf57991bb30b829d74653196ad13ca2a65f3
Joyce Muñoz <joyceaketzalli@gmail.com>
2022-11-22 14:12:48 -0600

    Agrega la vista de Login con su CSS

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import './Login.css';
+
+function Login(){
+    return (
+        <div className="Login">
+            <h1>All Notes</h1>
+            <p>Click on the ‘write’ icon to get started</p>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/components/MainView.jsx
@@ -0,0 +1,5 @@
+import './MainView.css';
+
+function MainView(){
+    
+}
