7a7c94fd635a5ffb702da5bc8b2669ece7e1761d
Joyce Muñoz <joyceaketzalli@gmail.com>
2022-12-04 13:35:16 -0600

    feature: First steps for routing within the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Login from './components/noauth/Login';
 import MainView from "./components/auth/MainView";
-import { NotesView } from "./components/auth/NotesView";
+import NotesView from './components/auth/NotesView';
 
 
 
 function App() {
-  return (
-    <NotesView/>
+  const { user, setUSer } = useState(null);
 
+  if (!user) {
+    return <Login />;
+  }
+
+  return (
+    <Routes>
+      <Route path='/main' element={<MainView />} />
+      <Route path='/notes' element={<NotesView />} />
+    </Routes>
   );
 }
-
+ 
 export default App; 
